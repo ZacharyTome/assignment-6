623da3e99a41615db57f30b37e1241a224d6c87b
Zachary Tome <zackktome@gmail.com>
2025-04-04 17:26:47 -0400

    Update test_fibonacci.py

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,1 +1,29 @@
+import unittest
+from fibonacci import Fibonacci
 
+class TestFibonacci(unittest.TestCase):
+
+    def test_fibonacci_non_integer(self):
+        with self.assertRaises(ValueError):
+            Fibonacci("one")
+
+    def test_fibonacci_0(self):
+        self.assertEqual(list(Fibonacci(0)), [0])
+
+    def test_fibonacci_1(self):
+        self.assertEqual(list(Fibonacci(1)), [0, 1])
+
+    def test_fibonacci_2(self):
+        self.assertEqual(list(Fibonacci(2)), [0, 1, 1])
+
+    def test_fibonacci_4(self):
+        self.assertEqual(list(Fibonacci(4)), [0, 1, 1, 2, 3])
+
+    def test_fibonacci_10(self):
+        self.assertEqual(list(Fibonacci(10)), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+
+    def test_fibonacci_negative(self):
+        self.assertEqual(list(Fibonacci(-1)), [])
+
+if __name__ == '__main__':
+    unittest.main()
